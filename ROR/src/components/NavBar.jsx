b2eb18fa315c14e387eaa3494d74c24a64e1159a
Sudharsan-R23 <sudharsanr796@gmail.com>
2026-01-08 00:28:12 +0530

    fix: added the components

--- ROR/src/components/NavBar.jsx
+++ ROR/src/components/NavBar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import styles from '../styles/NavBar.module.css'
+import Logo from '../assets/ubisoft.png'
 
 export default function NavBar() {
   return (
-    <div className={styles.main}>NavBar</div>
+    <nav className={styles.NavContainer}>
+      <div className={styles.LogoContainer}>
+        <img src={Logo} alt="" />
+      </div>
+
+      <dir className={styles.NavContxt}>
+         
+      </dir>
+    </nav>
   )
 }
