9302f0da9f27287d9bdcafe4823beeecc9f83fa2
Sudharsan-R23 <sudharsanr796@gmail.com>
2026-01-14 12:26:15 +0530

    Fix: added the component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,58 @@
 import styles from '../styles/Home.module.css';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Banner from '../assets/B1.webp';
 import Banner2 from '../assets/B2.webp';
 import jc from '../assets/jc.webp';
+import bmusic from '../assets/music.mp3'
+import { MdMusicNote, MdMusicOff } from "react-icons/md";
 import { FaSteam, FaPlaystation, FaXbox, FaGooglePlay } from "react-icons/fa";
 import { SiEpicgames } from "react-icons/si";
 import { IoLogoAppleAppstore } from "react-icons/io5";
 
 export default function Home() {
-  
-  const [open, setOpen] =useState(false);
+
+  const [open, setOpen] = useState(false);
+  const audioRef = useRef(null);
+
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const toggleMusic = () => {
+    if (!audioRef.current) return;
+
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+
+    setIsPlaying(!isPlaying);
+  }
 
   return (
     <div className={styles.Hcontainer}>
+      <audio ref={audioRef} src={bmusic} loop preload="auto" />
+
+      <button className={styles.musicBtn} onClick={toggleMusic}>
+        {isPlaying ? <MdMusicNote /> : <MdMusicOff />}
+      </button>
       <section className={styles.Home}>
 
         <div className={styles.HeadDev}>
-          <img src={Banner} alt="" loading="lazy"/>
+          <img src={Banner} alt="" loading="lazy" />
 
           <div className={styles.HomeContxt}>
-  
+
             <h1>REVENGE OF REALMS</h1>
             <p>A king will fight long for empire</p>
 
             <div className={styles.ButtonContxt}>
               <button>PLAY</button>
-              <button onClick={()=>{setOpen(!open)}}>MOBILE</button>
+              <button onClick={() => { setOpen(!open) }}>MOBILE</button>
             </div>
-             <div className={`${styles.MobileContxt} ${open ? styles.open :''}`}>
-                <FaGooglePlay />
-                <IoLogoAppleAppstore />
-              </div>
+            <div className={`${styles.MobileContxt} ${open ? styles.open : ''}`}>
+              <FaGooglePlay />
+              <IoLogoAppleAppstore />
+            </div>
 
             <div className={`${styles.PlayIcons} ${open ? styles.hide : ''}`}>
               <FaSteam />
@@ -43,9 +65,9 @@ export default function Home() {
         </div>
       </section>
       <section className={styles.Game}>
-      <img className={styles.GameBanner} src={Banner2} alt="" />
+        <img className={styles.GameBanner} src={Banner2} alt="" />
         <div className={styles.GameContxt}>
-          <img className={styles.GameImg} src={jc} alt=""/>
+          <img className={styles.GameImg} src={jc} alt="" />
           <div className={styles.GameItem}>
             <h1>Lorem ipsum doxercitationem.</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt deleniti est quisquam velit commodi, natus soluta id sit, odio dignissimos qui adipisci suscipit! Voluptate cumque quas, quasi nemo ad ipsam magni optio officiis aperiam cupiditate quibusdam voluptatem ratione consequuntur hic nulla, quaerat error totam nesciunt, id harum numquam omnis expedita.</p>
@@ -54,7 +76,7 @@ export default function Home() {
         </div>
       </section>
       <section className={styles.section3}>
-        
+
       </section>
     </div>
   )
