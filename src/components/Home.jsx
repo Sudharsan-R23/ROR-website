d92fab9cb5c47e883e97b8b0bc8685f573756390
Sudharsan-R23 <sudharsanr796@gmail.com>
2026-01-15 12:50:18 +0530

    Fix: added the components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,8 @@ import { useState, useRef, useEffect } from 'react';
 import Banner from '../assets/B1.webp';
 import Banner2 from '../assets/B2.webp';
 import jc from '../assets/jc.webp';
+import jc2 from '../assets/godL.webp'
+import gb2 from '../assets/Game2Banner.webp'
 import { FaSteam, FaPlaystation, FaXbox, FaGooglePlay } from "react-icons/fa";
 import { SiEpicgames } from "react-icons/si";
 import { IoLogoAppleAppstore } from "react-icons/io5";
@@ -16,7 +18,7 @@ export default function Home() {
 
   return (
     <div className={styles.Hcontainer}>
-      
+
       <section className={styles.Home}>
 
         <div className={styles.HeadDev}>
@@ -57,8 +59,16 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className={styles.section3}>
-
+      <section className={styles.Game2}>
+        <img className={styles.GameBanner} src={gb2} alt="" />
+        <div className={`${styles.GameContxt} ${styles.Game2Contxt}`}>
+          <div className={styles.GameItem}>
+            <h1>Lorem ipsum doxercitationem.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt deleniti est quisquam velit commodi, natus soluta id sit, odio dignissimos qui adipisci suscipit! Voluptate cumque quas, quasi nemo ad ipsam magni optio officiis aperiam cupiditate quibusdam voluptatem ratione consequuntur hic nulla, quaerat error totam nesciunt, id harum numquam omnis expedita.</p>
+            <p>rferendis numquam unde in distinctio iste ducimus doloribus, dignissimos dolorum natus modi aut nam aliquid. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, labore illum saepe culpa officia facilis et deserunt nostrum, id voluptatibus, nihil itaque blanditiis. Et suscipit placeat error deleniti aspernatur ut.</p>
+          </div>
+          <img className={styles.GameImg} src={jc2} alt="" />
+        </div>
       </section>
     </div>
   )
