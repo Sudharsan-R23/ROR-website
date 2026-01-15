3761a554c328230907baa0fcb5b028e18a61601f
Sudharsan-R23 <sudharsanr796@gmail.com>
2026-01-15 22:55:45 +0530

    Fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { React } from 'react';
-import Logo from '../assets/ubisoft.webp';
+import Logo from '../assets/ubisoft.png';
+import Unreal from '../assets/Unreal.png';
 import styles from '../styles/Footer.module.css';
 import FooterBanner from '../assets/FooterB.jpg';
 import { FaInstagram, FaXTwitter } from 'react-icons/fa6'
@@ -17,6 +18,10 @@ export default function Footer() {
     <div className={styles.Footertxt}>
       <img src={FooterBanner} alt="" />
       <div className={styles.FooterSec1}>
+        <div className={styles.FooterLogo}>
+          <img src={Logo} alt="" />
+          <img src={Unreal} alt="" />
+        </div>
         <div className={styles.Icons}>
           <h1>JOINT NOW |</h1>
           <FaInstagram onClick={() => openLink('https://instagram.com')} />
@@ -24,12 +29,9 @@ export default function Footer() {
           <FiYoutube onClick={() => openLink('https://youtube.com')} />
           <AiOutlineLinkedin onClick={() => openLink('https://youtube.com')} />
         </div>
-        <div className={styles.FooterLogo}>
-          <img src={Logo} alt="" />
-          <img src={Logo} alt="" />
-        </div>
+        
         <div className={styles.FooterPara}>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab voluptatibus iste facilis vitae illo perferendis impedit sit ipsam enim excepturi! Totam doloribus, cupiditate amet id eum veritatis porro, placeat eligendi architecto omnis nisi atque dolor ipsa ipsum minus ipsam iste praesentium, blanditiis voluptatem dolorem facere laborum obcaecati animi! Ratione delectus eveniet cupiditate quas necessitatibus quidem repellendus distinctio repellat at, facere suscipit, eaque aliquid neque, voluptate harum a explicabo ipsum error quis quae ex magnam. Rerum molestias illo impedit facilis veritatis quaerat magni nulla non libero, vero.</p>
+          <p> s illo impedit facilis veritatis quaerat magni nulla non libero, vero.</p>
         </div>
       </div>
 
